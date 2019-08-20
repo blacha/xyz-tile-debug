0d3ed99c06af291753a3bc13bbbb180083af85af
Blayne Chard <blayne@chard.com>
2019-08-20 16:40:12 +1200

    feat: split zoom onto seperate line to allow for higher zoom levels

--- src/img.ts
+++ src/img.ts
@@ -3,7 +3,7 @@ import { QuadKey } from './quad.key';
 import * as NodeCanvas from 'canvas';
 
 const CANVAS_SIZE = 256;
-const FONT_SIZE = CANVAS_SIZE / 8;
+const FONT_SIZE = CANVAS_SIZE / 6;
 
 async function xyzPng(v: Vector) {
     let qk = QuadKey.toQuadKey(v);
@@ -11,9 +11,12 @@ async function xyzPng(v: Vector) {
     if (qk.length > 10) {
         qk = qk.slice(0, 3) + '..' + qk.slice(qk.length - 7);
     }
-    const xyzS = `${v.x},${v.y} z${v.z}`;
+    const xyzS = `${v.x},${v.y}`;
     const canvas = NodeCanvas.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
     const ctx = canvas.getContext('2d');
+
+    const halfCanvas = CANVAS_SIZE / 2;
+    const quarterCanvas = CANVAS_SIZE / 4;
 
     // Fill & Stroke the center text
     ctx.textAlign = 'center';
@@ -21,10 +24,12 @@ async function xyzPng(v: Vector) {
     ctx.font = `bold ${FONT_SIZE}px "m+ 1m"`;
     ctx.fillStyle = 'rgba(255,255,255,1)';
     ctx.strokeStyle = 'rgba(0,0,0,1)';
-    ctx.fillText(xyzS, CANVAS_SIZE / 2, CANVAS_SIZE / 2 + FONT_SIZE / 2);
-    ctx.strokeText(xyzS, CANVAS_SIZE / 2, CANVAS_SIZE / 2 + FONT_SIZE / 2);
-    ctx.fillText(qk, CANVAS_SIZE / 2, CANVAS_SIZE / 2 - FONT_SIZE / 2);
-    ctx.strokeText(qk, CANVAS_SIZE / 2, CANVAS_SIZE / 2 - FONT_SIZE / 2);
+    ctx.fillText(xyzS, halfCanvas, quarterCanvas);
+    ctx.strokeText(xyzS, halfCanvas, quarterCanvas);
+    ctx.fillText(qk, halfCanvas, quarterCanvas * 2);
+    ctx.strokeText(qk, halfCanvas, quarterCanvas * 2);
+    ctx.fillText(`z${v.z}`, halfCanvas, quarterCanvas * 3);
+    ctx.strokeText(`z${v.z}`, halfCanvas, quarterCanvas * 3);
 
     // Stroke around the edges
     ctx.strokeStyle = 'rgba(255,0,0,0.75)';
