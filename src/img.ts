c8a89b2253a9d3826c06dbe7d3ab3c4a9d438aa7
Blayne Chard <blayne@chard.com>
2020-07-11 13:17:32 +1200

    feat: color code images based on tile matrix set

--- src/img.ts
+++ src/img.ts
@@ -3,28 +3,33 @@ import { QuadKey, EpsgCode } from '@basemaps/geo';
 import { TileMatrixSet } from '@basemaps/geo/build/tile.matrix.set';
 import * as NodeCanvas from 'canvas';
 
-const CANVAS_SIZE = 256;
-const FONT_SIZE = CANVAS_SIZE / 6;
+const CanvasSize = 256;
+const FontSize = CanvasSize / 6;
+
+const BorderColors: Record<number, string> = {
+    [EpsgCode.Google]: 'rgba(255,0,0,0.75)',
+    [EpsgCode.Nztm2000]: 'rgba(255,0,255,0.75)',
+};
 
 async function xyzPng(v: Vector, tms?: TileMatrixSet) {
-    let qk = tms == null || tms?.projection.code == EpsgCode.Google ? QuadKey.fromTile(v) : '';
+    const epsg = tms?.projection.code ?? EpsgCode.Google;
+    let qk = epsg == EpsgCode.Google ? QuadKey.fromTile(v) : '';
 
     // Quad keys get too long for tiles so shorten them
-    if (qk.length > 10) {
-        qk = qk.slice(0, 3) + '.' + qk.slice(qk.length - 6);
-    }
+    if (qk.length > 10) qk = qk.slice(0, 3) + '.' + qk.slice(qk.length - 6);
+
     const xyzS = `${v.x},${v.y}`;
-    const canvas = NodeCanvas.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+    const canvas = NodeCanvas.createCanvas(CanvasSize, CanvasSize);
     const ctx = canvas.getContext('2d');
 
-    const halfCanvas = CANVAS_SIZE / 2;
-    const quarterCanvas = CANVAS_SIZE / 4;
+    const halfCanvas = CanvasSize / 2;
+    const quarterCanvas = CanvasSize / 4;
 
     // Fill & Stroke the center text
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.font = `bold ${FONT_SIZE}px "m+ 1m"`;
-    ctx.fillStyle = 'rgba(255,255,255,1)';
+    ctx.font = `bold ${FontSize}px "m+ 1m"`;
+    ctx.fillStyle = 'rgba(240,240,240,1)';
     ctx.strokeStyle = 'rgba(0,0,0,1)';
     // Render the XYZ
     ctx.fillText(xyzS, halfCanvas, halfCanvas - quarterCanvas);
@@ -40,20 +45,20 @@ async function xyzPng(v: Vector, tms?: TileMatrixSet) {
 
     // Log the TileMatrixSet used to render this
     if (tms) {
-        ctx.font = `bold ${FONT_SIZE - 12}px "m+ 1m"`;
+        ctx.font = `bold ${FontSize - 12}px "m+ 1m"`;
         ctx.textBaseline = 'bottom';
 
-        ctx.fillText(tms.def.identifier, halfCanvas, CANVAS_SIZE);
-        ctx.strokeText(tms.def.identifier, halfCanvas, CANVAS_SIZE);
+        ctx.fillText(tms.def.identifier, halfCanvas, CanvasSize);
+        ctx.strokeText(tms.def.identifier, halfCanvas, CanvasSize);
     }
 
     // Stroke around the edges
-    ctx.strokeStyle = 'rgba(255,0,0,0.75)';
+    ctx.strokeStyle = BorderColors[epsg] || BorderColors[EpsgCode.Google];
     ctx.beginPath();
     ctx.lineTo(0.5, 0.5);
-    ctx.lineTo(0.5, CANVAS_SIZE - 0.5);
-    ctx.lineTo(CANVAS_SIZE - 0.5, CANVAS_SIZE - 0.5);
-    ctx.lineTo(CANVAS_SIZE - 0.5, 0.5);
+    ctx.lineTo(0.5, CanvasSize - 0.5);
+    ctx.lineTo(CanvasSize - 0.5, CanvasSize - 0.5);
+    ctx.lineTo(CanvasSize - 0.5, 0.5);
     ctx.lineTo(0.5, 0.5);
     ctx.stroke();
 
