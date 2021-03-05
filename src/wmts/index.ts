6f2203282ada5c09450ea1fd1eaaa34429011c61
Blayne Chard <blayne@chard.com>
2021-03-05 14:57:55 +1300

    feat: support NZTM2000Quad TileMatrix

--- src/wmts/index.ts
+++ src/wmts/index.ts
@@ -1,9 +1,14 @@
-import { Nztm2000Tms } from '@basemaps/geo/build/tms/nztm2000';
+import * as NZTM from '@linzjs/tile-matrix-set';
 import { GoogleTms } from '@basemaps/geo/build/tms/google';
+import {TileMatrixSet} from '@basemaps/geo'
 
-export const TileMatrixes = [Nztm2000Tms, GoogleTms];
+const Nztm2000Tms = new TileMatrixSet(NZTM.Nztm2000);
+const Nztm2000QuadTms = new TileMatrixSet(NZTM.Nztm2000Quad)
+
+export const TileMatrixes = [Nztm2000Tms, Nztm2000QuadTms, GoogleTms];
 
 export const BorderColors: Record<string, string> = {
   [GoogleTms.def.identifier]: 'rgba(255,0,0,0.75)',
   [Nztm2000Tms.def.identifier]: 'rgba(255,0,255,0.75)',
+  [Nztm2000QuadTms.def.identifier]: 'rgba(255,127,63,0.75)',
 };
