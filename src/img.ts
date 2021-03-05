ce68c562b5ff9f6d46b84bd02b01c018d4b20a2b
Blayne Chard <blayne@chard.com>
2021-03-05 17:03:12 +1300

    feat: render tiles at higher resolution

--- src/img.ts
+++ src/img.ts
@@ -1,17 +1,24 @@
-import { EpsgCode, QuadKey } from '@basemaps/geo';
+import { QuadKey } from '@basemaps/geo';
 import { TileMatrixSet } from '@basemaps/geo/build/tile.matrix.set';
 import { GoogleTms } from '@basemaps/geo/build/tms/google';
 import * as NodeCanvas from 'canvas';
 import { Vector } from './vector';
 import { BorderColors } from './wmts';
 
-const CanvasSize = 256;
-const FontSize = CanvasSize / 6;
+const CanvasSize = 512;
+const FontSize = CanvasSize / 8;
+
+function isQuadKeyCapable(tms: TileMatrixSet): boolean {
+  for (const def of tms.def.tileMatrix) {
+    if (def.matrixWidth !== def.matrixHeight) return false;
+  }
+  return true;
+}
 
 async function toXyz(v: Vector, tms?: TileMatrixSet): Promise<Buffer> {
-  const epsg = tms?.projection.code ?? EpsgCode.Google;
   const espgName = tms?.def.identifier ?? GoogleTms.def.identifier;
-  let qk = epsg === EpsgCode.Google ? QuadKey.fromTile(v) : '';
+
+  let qk = isQuadKeyCapable(tms ?? GoogleTms) ? QuadKey.fromTile(v) : '';
 
   // Quad keys get too long for tiles so shorten them
   if (qk.length > 10) qk = qk.slice(0, 3) + '.' + qk.slice(qk.length - 6);
@@ -26,7 +33,7 @@ async function toXyz(v: Vector, tms?: TileMatrixSet): Promise<Buffer> {
   // Fill & Stroke the center text
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-  ctx.font = `bold ${FontSize}px "m+ 1m"`;
+  ctx.font = `bold ${FontSize}px 'm+ 1m'`;
   ctx.fillStyle = 'rgba(240,240,240,1)';
   ctx.strokeStyle = 'rgba(0,0,0,1)';
   // Render the XYZ
@@ -34,8 +41,10 @@ async function toXyz(v: Vector, tms?: TileMatrixSet): Promise<Buffer> {
   ctx.strokeText(xyzS, halfCanvas, halfCanvas - quarterCanvas);
 
   // Quadkey
-  ctx.fillText(qk, halfCanvas, halfCanvas);
-  ctx.strokeText(qk, halfCanvas, halfCanvas);
+  if (qk !== '') {
+    ctx.fillText(qk, halfCanvas, halfCanvas);
+    ctx.strokeText(qk, halfCanvas, halfCanvas);
+  }
 
   // Zoom level
   ctx.fillText(`z${v.z}`, halfCanvas, halfCanvas + quarterCanvas);
@@ -45,6 +54,7 @@ async function toXyz(v: Vector, tms?: TileMatrixSet): Promise<Buffer> {
   if (tms) {
     ctx.font = `bold ${FontSize - 12}px "m+ 1m"`;
     ctx.textBaseline = 'bottom';
+    ctx.fillStyle = BorderColors[espgName];
 
     ctx.fillText(tms.def.identifier, halfCanvas, CanvasSize);
     ctx.strokeText(tms.def.identifier, halfCanvas, CanvasSize);
@@ -52,6 +62,7 @@ async function toXyz(v: Vector, tms?: TileMatrixSet): Promise<Buffer> {
 
   // Stroke around the edges
   ctx.strokeStyle = BorderColors[espgName];
+
   ctx.beginPath();
   ctx.lineTo(0.5, 0.5);
   ctx.lineTo(0.5, CanvasSize - 0.5);
